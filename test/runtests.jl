3004b3a00817b9561e49e4a09fcdcb7aef75dbd8
viniciuspiccoli <viniciuspiccoli2008@hotmail.com>
2021-01-08 11:53:45 -0300

    fpair fixed

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,19 @@
 using simulationQP934
 using Test
 
+
+
+
+
 @testset "simulationQP934.jl" begin
     # Write your tests here.
     data = Data();
     box = initial_point(data);
     nc, fatm, natm = linkedlist(box,data);
     @test utotal(box, data, fatm, natm, nc) \approx utotal(box,data)
+
+    upair,f = fpair(box[1],box[2],r,data)    
+    @test upair \approx  -0.2955814601750326   
+
 
 end
